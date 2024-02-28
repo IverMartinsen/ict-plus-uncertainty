4563204969366afb44f9749fe02145cd1681e042
Iver Martinsen <iver.martinsen@outlook.com>
2024-02-28 18:58:22 +0100

    add lr schedule

--- train_model.py
+++ train_model.py
@@ -72,17 +72,33 @@ if __name__ == "__main__":
     ds_val = ds_val.batch(args.batch_size)    
     
     base_model = tf.keras.applications.EfficientNetB0(include_top=False, weights=None, pooling='avg')
+    wandb.config.update({'base_model': base_model.name})
     model = tf.keras.Sequential([
         base_model,
         tf.keras.layers.Dense(4, activation='softmax')
     ])    
     
-    optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=args.learning_rate, weight_decay=args.weight_decay)
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=args.learning_rate, 
+        decay_steps=args.epochs*len(ds_train), 
+        decay_rate=10, 
+        staircase=False,
+        )
+    
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+    # also monitor the learning rate
+    lr_variable = wandb.define_metric('learning_rate')
+    
+    def log_lr(epoch, logs):
+        wandb.log({'learning_rate': optimizer._decayed_lr(tf.float32).numpy()}, commit=False)
+    
+    lr_callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=log_lr)
+    
     model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(
         ds_train, 
         epochs=args.epochs, 
         validation_data=ds_val, 
-        callbacks=[wandb.keras.WandbCallback(save_model=False)],
+        callbacks=[wandb.keras.WandbCallback(save_model=False), lr_callback]
         )
     
