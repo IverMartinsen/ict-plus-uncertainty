aa41e1ed98f1a90d7e698152d6d4f4cf0610579c
Iver Martinsen <iver.martinsen@outlook.com>
2024-06-18 13:13:06 +0200

    a little cleanup

--- visualize_uncertainty.py
+++ visualize_uncertainty.py
@@ -1,3 +1,4 @@
+# Lots of boilerplate code to visualize the uncertainty of the model
 import os
 import numpy as np
 import pandas as pd
@@ -21,10 +22,8 @@ if __name__ == '__main__':
     df = pd.read_csv(path_to_df)
 
     # =============================================================================
-    # UNCERTAINTY SCATTER PLOTS
+    # TOTAL VARIANCE AGAINTS PREDICTIVE VARIANCE
     # =============================================================================
-
-
     x = df['predictive_variance']
     y = df['total_variance']
 
@@ -33,7 +32,9 @@ if __name__ == '__main__':
     plt.xlabel('Variance for the predicted class')
     plt.ylabel('Total Variance for all classes')
     plt.savefig(os.path.join(destination, 'predictive_variance_vs_total_variance.pdf'), bbox_inches='tight', dpi=300)
-
+    # =============================================================================
+    # LOSS AGAINST IQR
+    # =============================================================================
     x = df['iqr'] + 1e-4
     x = np.log(x)
     y = df['loss']
@@ -47,8 +48,9 @@ if __name__ == '__main__':
     ax.set_title('IQR vs Loss', fontsize=10, fontweight='bold')
     ax.legend(['correct', '0.50 quantile', '0.75 quantile'])
     plt.savefig(os.path.join(destination, 'iqr.png'), bbox_inches='tight', dpi=300)
-
-
+    # =============================================================================
+    # LOSS AGAINST TOTAL VARIANCE
+    # =============================================================================
     x = df['total_variance']
     y = df['loss']
 
@@ -61,19 +63,11 @@ if __name__ == '__main__':
     ax.set_title('Total variance vs Loss', fontsize=10, fontweight='bold')
     ax.legend(['correct', '0.50 quantile', '0.75 quantile'])
     plt.savefig(os.path.join(destination, 'total_variance.png'), bbox_inches='tight', dpi=300)
-
-
-
-    percentage = 0.75
-
-    ep_cov, al_cov = compute_predictive_variance(Y_pred)
-    cov = ep_cov + al_cov
-    eigvals = np.sort(np.linalg.eigvals(cov), axis=1)[:, ::-1]
-
-    x = cov[:, np.arange(4), np.arange(4)] # diagonal of the covariance matrix
-    idx = np.argmax(Y_pred.mean(axis=1), axis=1) # get the index of the maximum prediction
-    x = x[np.arange(len(df['label'])), idx] # get the diagonal of the covariance matrix at the maximum prediction
-
+    # =============================================================================
+    # LOSS AGAINST PREDICTIVE VARIANCE
+    # =============================================================================
+    x = df['predictive_variance']
+    y = df['loss']
     # compute number of wrong predictions below the 75th percentile
     threshold = np.quantile(x, percentage)
     score = np.array((df['label'] != df['pred_mean'])[x < threshold]).sum()
@@ -84,54 +78,52 @@ if __name__ == '__main__':
         loss=df['loss'],
         c=df['label'] == df['pred_mean'],
         x_label='Predictive Variance',
-        title=f'Predictive Variance vs Loss ({int(score)}/{int(total_wrong)} below {percentage} quantile)',
+        title=f'Predictive Variance vs Loss',
         destination=destination,
-        quantile_percentage=percentage,
         filename='predictive_variance.png'
         )
-
+    # =============================================================================
+    # LOSS AGAINST PERCENTAGE AGREE
+    # =============================================================================
     x = df['percentage_agree'].values.reshape(-1, 1)
     x = 1 - x
-    threshold = np.quantile(x, percentage)
-    score = np.array((df['label'] != df['pred_mean'])[x.flatten() < threshold]).sum()
     x += np.random.normal(0, 0.01, x.shape)
         
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.scatter(x, np.log(df['loss']), c=df['label'] == df['pred_mean'])
     ax.set_ylabel('Loss')
     ax.set_xlabel('1 - Percentage Agreement')
-    ax.axvline(x=threshold, color='black', linestyle='--')
-    ax.set_title(f'Percentage Agree vs Loss ({int(score)}/{int(total_wrong)} below {percentage} quantile)', fontsize=10, fontweight='bold')
-    ax.legend(['correct', f'{percentage} quantile'])
+    ax.axvline(x=np.quantile(x, 0.50), color='black', linestyle='--')
+    ax.avline(x=np.quantile(x, 0.75), color='black', linestyle='-.')
+    ax.set_title(f'Percentage Agree vs Loss', fontsize=10, fontweight='bold')
+    ax.legend(['correct', '0.50 quantile', '0.75 quantile'])
     plt.savefig(os.path.join(destination, 'percentage_agree.png'), bbox_inches='tight', dpi=300)
-
+    # =============================================================================
+    # LOSS AGAINST EPISTEMIC UNCERTAINTY
+    # =============================================================================
     x = df['epistemic_variance'].values.reshape(-1, 1)
-    threshold = np.quantile(x, percentage)
-    score = np.array((df['label'] != df['pred_mean'])[x.flatten() < threshold]).sum()
 
     plot_uncertainty(
         x=x,
         loss=df['loss'],
         c=df['label'] == df['pred_mean'],
         x_label='Epistemic Uncertainty',
-        title=f'Epistemic Uncertainty vs Loss ({int(score)}/{int(total_wrong)} below {percentage} quantile)',
+        title=f'Epistemic Uncertainty vs Loss',
         destination=destination,
-        quantile_percentage=percentage,
         filename='epistemic_uncertainty.png'
         )
-
+    # =============================================================================
+    # LOSS AGAINST ALEATORIC UNCERTAINTY
+    # =============================================================================
     x = df['aleatoric_variance'].values.reshape(-1, 1)
-    threshold = np.quantile(x, percentage)
-    score = np.array((df['label'] != df['pred_mean'])[x.flatten() < threshold]).sum()
 
     plot_uncertainty(
         x=x,
         loss=df['loss'],
         c=df['label'] == df['pred_mean'],
         x_label='Aleatoric Uncertainty',
-        title=f'Aleatoric Uncertainty vs Loss ({int(score)}/{int(total_wrong)} below {percentage} quantile)',
+        title=f'Aleatoric Uncertainty vs Loss',
         destination=destination,
-        quantile_percentage=percentage,
         filename='aleatoric_uncertainty.png'
         )    
     
