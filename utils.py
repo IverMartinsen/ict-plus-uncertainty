e594db374bd862ce41a742dd1e757cc6ed84b3a7
Iver Martinsen <iver.martinsen@outlook.com>
2024-09-09 13:39:28 +0200

    cleaned up script

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import scipy.stats
 from PIL import Image
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, cohen_kappa_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 lab_to_int = {'A': 0, 'B': 1, 'S': 2, 'P': 3}
 int_to_lab = {v: k for k, v in lab_to_int.items()}
@@ -80,42 +80,46 @@ def store_predictions(y_pred, y, filenames, destination, y_var=None, y_logits=No
     df['label'] = y
     for i in range(num_samples):
         df[f'model_{i}'] = y_pred[:, i, :].argmax(axis=1)
-    df['agree'] = np.prod(df.iloc[:, 2:] == df.iloc[:, 2].values[:, None], axis=1).astype(bool) # check if all models agree
+
     keys = [f'model_{i}' for i in range(num_samples)]
-    df['pred_mode'] = df[keys].mode(axis=1)[0] # majority vote
-    df['percentage_agree'] = df[keys].apply(lambda x: x.value_counts().max() / x.value_counts().sum(), axis=1)
-    df['pred_mean'] = y_pred.mean(axis=1).argmax(axis=1) # mean prediction
-    df['loss'] = -np.log(y_pred.mean(axis=1)[np.arange(len(y)), y])
-    df['conf_mean'] = y_pred.mean(axis=1).max(axis=1) # mean confidence
-    conf_median = np.median(y_pred, axis=1)
-    conf_median /= conf_median.sum(axis=1)[:, None]
-    df['conf_median'] = conf_median.max(axis=1)
-    df['pred_median'] = conf_median.argmax(axis=1)
-    conf_min = np.min(y_pred, axis=1)
-    conf_min = conf_min[np.arange(len(y)), df['pred_mean']]
-    df['conf_min'] = conf_min
-    df['var_mean'] = (df['conf_mean'] * (1 - df['conf_mean'])) # mean variance
-    iqr = scipy.stats.iqr(y_pred, axis=1)
-    iqr = iqr[np.arange(len(y)), df['pred_mean']]
-    df['iqr'] = iqr
     var = np.var(y_pred, axis=1)
-    var = var[np.arange(len(y)), df['pred_mean']]
-    df['var'] = var
+    iqr = scipy.stats.iqr(y_pred, axis=1)
     ep_cov, al_cov = compute_predictive_variance(y_pred)
     cov = ep_cov + al_cov
     cov = cov[:, np.arange(4), np.arange(4)]
-    df['predictive_variance'] = cov[np.arange(n), np.array(df['pred_mean']).flatten()]
-    #eigvals = np.sort(np.linalg.eigvals(cov), axis=1)[:, ::-1]
-    df['total_variance'] = cov.sum(axis=1)
-    df['epistemic_variance'] = ep_cov[:, np.arange(4), np.arange(4)].sum(axis=1)
-    df['aleatoric_variance'] = al_cov[:, np.arange(4), np.arange(4)].sum(axis=1)
+
+    # Percentage agreement
+    df['agree'] = np.prod(df.iloc[:, 2:] == df.iloc[:, 2].values[:, None], axis=1).astype(bool) # check if all models agree
+    df['percentage_agree'] = df[keys].apply(lambda x: x.value_counts().max() / x.value_counts().sum(), axis=1)
+    # Mean prediction
+    df['pred_mean'] = y_pred.mean(axis=1).argmax(axis=1) # mean prediction
+    df['loss_mean'] = -np.log(y_pred.mean(axis=1)[np.arange(len(y)), y])
+    df['conf_mean'] = y_pred.mean(axis=1).max(axis=1) # mean confidence
+    df['var_mean'] = var[np.arange(len(y)), df['pred_mean']]
+    df['iqr_mean'] = iqr[np.arange(len(y)), df['pred_mean']]
+    df['post_pred_var_mean'] = cov[np.arange(n), np.array(df['pred_mean']).flatten()]
+    # Mode prediction
+    df['pred_mode'] = df[keys].mode(axis=1)[0] # majority vote
+    # Median prediction
+    conf_median = np.median(y_pred, axis=1)
+    conf_median /= conf_median.sum(axis=1)[:, None]
+    df['pred_median'] = conf_median.argmax(axis=1)
+    df['loss_median'] = -np.log(conf_median[np.arange(len(y)), y])
+    df['conf_median'] = conf_median.max(axis=1)
+    df['var_median'] = var[np.arange(len(y)), df['pred_median']]
+    df['iqr_median'] = iqr[np.arange(len(y)), df['pred_median']]
+    df['post_pred_var_median'] = cov[np.arange(n), np.array(df['pred_median']).flatten()]
+    # Epistemic uncertainty
+    df['tot_epi_var'] = ep_cov[:, np.arange(4), np.arange(4)].sum(axis=1)
+    df['tot_ale_var'] = al_cov[:, np.arange(4), np.arange(4)].sum(axis=1)
+    df['tot_var'] = cov.sum(axis=1)
     if y_var is not None:
-        df['modeled_variance'] = y_var
+        df['modeled_var'] = y_var
     if y_logits is not None:
         df['logits'] = y_logits[np.arange(len(y)), 0, df['pred_mean']]
     else:
         y_logits = np.log(y_pred)
-        df['log_var'] = y_logits.var(axis=1)[np.arange(len(y)), df['pred_mean']]
+        df['log_var_mean'] = y_logits.var(axis=1)[np.arange(len(y)), df['pred_mean']]
     df.to_csv(os.path.join(destination, 'predictions.csv'), index=False)
     
     return df
@@ -126,6 +130,7 @@ def store_confusion_matrix(labels, predictions, destination):
     fig, ax = plt.subplots(figsize=(10, 10))
     disp.plot(ax=ax)
     plt.savefig(os.path.join(destination, 'confusion_matrix.png'), bbox_inches='tight', dpi=300)
+    plt.close()
 
 
 def plot_uncertainty(x, loss, c, x_label, title, filename, destination, q_val):
@@ -138,3 +143,4 @@ def plot_uncertainty(x, loss, c, x_label, title, filename, destination, q_val):
     ax.set_title(title, fontsize=10, fontweight='bold')
     ax.legend(['correct', '0.50 quantile', f'{q_val} quantile'])
     plt.savefig(os.path.join(destination, filename), bbox_inches='tight', dpi=300)
+    plt.close()
