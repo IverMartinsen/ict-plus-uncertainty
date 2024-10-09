380581b5fae44579e276d752256736d8b13dbb78
Iver Martinsen <iver.martinsen@outlook.com>
2024-10-09 12:46:13 +0200

    add argparse

--- compute_correlations.py
+++ compute_correlations.py
@@ -4,14 +4,19 @@ import pandas as pd
 from sklearn.metrics import cohen_kappa_score
 from scipy.stats import spearmanr
 
-path1 = "./stats/dropout_stats"
-path2 = "./results/swag_5e5_results"
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path1", type=str, default='./results/ensemble_results')
+parser.add_argument("--path2", type=str, default='./results/tta_results_s1')
+args = parser.parse_args()
+
+path1 = args.path1
+path2 = args.path2
 
 df1 = pd.read_csv(os.path.join(path1, 'predictions.csv'))
 df2 = pd.read_csv(os.path.join(path2, 'predictions.csv'))
 
-
-# replace // with /
 df1['filename'] = df1['filename'].apply(lambda x: os.path.basename(x))
 df2['filename'] = df2['filename'].apply(lambda x: os.path.basename(x))
 
@@ -26,8 +31,12 @@ y2 = df2['pred_mean']
 kappa = cohen_kappa_score(y1, y2)
 print(f"Cohen's Kappa: {kappa}")
 
-p1 = df1['conf_mean']
+p1 = df1['conf_median']
 p2 = df2['conf_mean']
 
 spearman = spearmanr(p1, p2)
 print(f"Spearman's Rho: {spearman.correlation}")
+
+# print filenames where mistakes are the same
+print("Same mistakes:")
+print(df1[(y1 != df1['label']) & (y2 != df2['label'])]['filename'].values)
