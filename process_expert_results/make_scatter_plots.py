44cbd249f8138dfca0af92722fb2d084a205e34c
Iver Martinsen <iver.martinsen@outlook.com>
2024-10-21 17:39:40 +0200

    plot mistakes vs uncertainty

--- process_expert_results/make_scatter_plots.py
+++ process_expert_results/make_scatter_plots.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from utils.utils import int_to_lab, lab_to_long
 
 destination = "./results/expert_results/"
 
@@ -33,3 +34,35 @@ for i, expert1 in enumerate(experts):
         plt.title(f'{expert1} vs {expert2}')
         plt.savefig(os.path.join(destination, f'{expert1}_vs_{expert2}.pdf'), dpi=300)
         plt.close()
+
+# ==============================
+# PLOT NUMBER OF MISTAKES VS UNCERTAINTY
+# ==============================
+
+markers = ['o', 'x', 's', '>']
+x = df['num_mistakes'] + np.random.normal(0, 0.1, len(df))
+y = df['mean_confidence'] + np.random.normal(0, 0.01, len(df))
+
+plt.figure(figsize=(10, 5))
+for i, label in enumerate(df['label'].unique()):
+    idx = df['label'] == label
+    plt.scatter(x[idx], y[idx], s=15, label=lab_to_long[int_to_lab[label]], marker=markers[i])
+plt.xlabel('Number of mistakes')
+plt.ylabel('Mean confidence')
+plt.legend()
+plt.savefig(os.path.join(destination, 'num_mistakes_vs_mean_confidence.pdf'), dpi=300)
+plt.close()
+
+markers = ['o', 'x', 's', '>']
+x = df['num_mistakes'] + np.random.normal(0, 0.1, len(df))
+y = df['weighted_confidence'] + np.random.normal(0, 0.01, len(df))
+
+plt.figure(figsize=(10, 5))
+for i, label in enumerate(df['label'].unique()):
+    idx = df['label'] == label
+    plt.scatter(x[idx], y[idx], s=15, label=lab_to_long[int_to_lab[label]], marker=markers[i])
+plt.xlabel('Number of mistakes')
+plt.ylabel('Weighted confidence')
+plt.legend()
+plt.savefig(os.path.join(destination, 'num_mistakes_vs_weighted_confidence.pdf'), dpi=300)
+plt.close()
